89833ba949170b86e7e4430f521696bb0a1fa65b
justpekka <ariefkhrisna631@gmail.com>
2022-02-09 09:46:27 +0700

    calculator 0.0.1Alpha done.

--- calc/script.js
+++ calc/script.js
@@ -1,25 +1,56 @@
 let result = document.querySelector("#result");
 let formula = document.querySelector("#formula");
 
-function display(a = undefined) {
-  formula.innerHTML += a;
+let query;
+let calcStatus;
+
+function display(a) {
+  if(calcStatus) {
+    query = undefined;
+    if(result.classList == "result") result.classList = 'formula';
+    if(formula.classList == "formula") formula.classList = 'result';
+    calcStatus = false;
+  };
+  query == undefined ? query = a : query += a;
+  formula.innerHTML = query;
+  update();
 }
 
-function operator(a = undefined) {
-  if(formula.innerHTML == "+" || "-" || "*" || "/" ) formula.innerHTML[-1] = a;
-  formula.innerHTML += a;
+function operator(a) {
+  if(calcStatus) {
+    query = undefined;
+    if(result.classList == "result") result.classList = 'formula';
+    if(formula.classList == "formula") formula.classList = 'result';
+    calcStatus = false;
+  };
+  if (query == undefined) query = 0+a;
+  // query.search(/\+\-\*\//i) ? query.replace(/\+\-\*\//i, a) : query += a;
+  query += a;
+  
+  formula.innerHTML = query;
+}
 
+function update() {
+  let updating = eval(query);
+  updating == undefined ? result.innerHTML = 0 : result.innerHTML = updating;
 }
 
 function calc() {
-  result.innerHTML = eval(formula.value);
+  calcStatus = true;
+  if(result.classList == "formula") result.classList = 'result';
+  if(formula.classList == "result") formula.classList = 'formula';
 };
 
 function del() {
-  formula.value -= 1;
+  query == undefined ? query = 0 : query.slice(-1);
+  formula.innerHTML = query;
 };
 
-function clear() {
-  formula.value = 0;
+function clr() {
+  query = 0;
+  formula.innerHTML = query;
+  if(result.classList == "result") result.classList = 'formula';
+  if(formula.classList == "formula") formula.classList = 'result';
+  update();
 };
 
